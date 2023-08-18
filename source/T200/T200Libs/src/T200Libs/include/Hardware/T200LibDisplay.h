8ccf15ec154a5034e9a0e4d2779e1938876b106f
jdayskynet <jdayskynet@proton.me>
2023-08-18 22:49:24 +0900

    modify

---
+++ source/T200/T200Libs/src/T200Libs/include/Hardware/T200LibDisplay.h
@@ -0,0 +1,16 @@
+#ifndef T200LIBDISPLAY_H
+#define T200LIBDISPLAY_H
+
+
+class T200LibDisplay
+{
+    public:
+        T200LibDisplay();
+        virtual ~T200LibDisplay();
+
+    protected:
+
+    private:
+};
+
+#endif // T200LIBDISPLAY_H
---
+++ source/T200/T200Libs/src/T200Libs/src/Hardware/T200LibDisplay.cpp
@@ -0,0 +1,11 @@
+#include "T200LibDisplay.h"
+
+T200LibDisplay::T200LibDisplay()
+{
+    //ctor
+}
+
+T200LibDisplay::~T200LibDisplay()
+{
+    //dtor
+}
