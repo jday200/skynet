0011edf15a0806514be1d9ec5a68258f65e38ef1
jday200 <skynet_lions@proton.me>
2023-08-30 23:20:46 +0900

    modify

---
+++ dev/training/react/lesson3/models/person.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class person extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  person.init({
+    mobile: DataTypes.STRING,
+    password: DataTypes.STRING,
+    type: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'person',
+  });
+  return person;
+};
