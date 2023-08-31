d24fb48694bc270767c850ad89eaaf746e564e74
jday200 <skynet_lions@proton.me>
2023-08-31 22:58:12 +0900

    modify

--- dev/training/react/lesson3/src/index.js
+++ dev/training/react/lesson3/src/index.js
@@ -6,9 +6,20 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <div>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  <div>
+      <form action="/save">
+        <label>Username:</label>
+        <input type="text" name="username"></input>
+        <label>Password:</label>
+        <input type="text" name="password"></input>
+        <input type="submit"></input>
+      </form>
+    </div>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
