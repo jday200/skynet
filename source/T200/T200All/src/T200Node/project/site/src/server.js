9921722f65ca14b87fac5f85d15b6e66e770d88a
jday200 <skynet_lions@proton.me>
2023-09-01 21:47:39 +0900

    modify

--- source/T200/T200All/src/T200Node/project/site/src/server.js
+++ source/T200/T200All/src/T200Node/project/site/src/server.js
@@ -1,4 +1,8 @@
-class SiteServer{
+//import T200NodeWebServer;
+const temp = require('../../../suite/web/T200NodeWebServer.js');
+
+
+class SiteServer extends T200NodeWebServer{
     constructor() {
         
     }
