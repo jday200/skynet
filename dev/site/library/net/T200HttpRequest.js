09bc7ba85156be92c90b6a68fef8ac09bbc9286d
jday200 <skynet_lions@proton.me>
2023-09-16 16:44:49 +0900

    modify

--- dev/site/library/net/T200HttpRequest.js
+++ dev/site/library/net/T200HttpRequest.js
@@ -2,32 +2,50 @@ const T200HttpResource = require('./T200HttpResource.js');
 
 class T200HttpRequest {
 	constructor() {
-		this.actions = {};
+		this.get = {};
+		this.post = {};
 	}
 	
-	run(action, req, res) {
+	run(method, action, req, res) {
+		if('get' === method) {
+			this.execute(this.get, action, req, res);
+		}else if('post' === method) {
+			this.execute(this.post, action, req, res);
+		}
+
+	}
+
+	execute(actions, action, req, res) {
 		var result;
-		
-		result = this.actions[action];
-		
+
+		result = actions[action];
+
 		if(result){
 			var HttpResource = new T200HttpResource();
 
-			var flag = HttpResource.exsits(result);
-			
+			var flag = HttpResource.exists(result);
+
 			if(flag){
-				var done = HttpResource.load(result);
-				
-				if(done){
-					done();	
-				}	
+				HttpResource.load_js(result);
 			}else{
-				
-			}			
-			 
-			return null;
+
+			}
 		}else{
-			return 1;	
+			var HttpResource = new T200HttpResource();
+
+			var flag = HttpResource.exists(action);
+
+			if(flag){
+				HttpResource.load(action, function(err, data){
+					if(err)throw err;
+					res.writeHead(200);
+					res.end(data);
+				});
+			}else{
+				res.writeHead(404);
+				res.end();
+			}
+
 		}
 	}
 	
--- dev/site/library/net/T200HttpResource.js
+++ dev/site/library/net/T200HttpResource.js
@@ -14,6 +14,21 @@ class T200HttpResource {
 		
 		fs.readFile(real, callback);
 	}
+
+	async load_js(file) {
+		var name = path.join("", file);
+		var real = path.join(__dirname, name);
+
+		console.log(real);
+
+		if(fs.existsSync(real)) {
+			var result = require(real);
+
+			if(result){
+				result();
+			}
+		}
+	}
 }
 
 module.exports = T200HttpResource;
