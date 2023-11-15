df358f73b7586ef7f66bae3e396794c8cc2cdd06
jday200 <skynet_lions@proton.me>
2023-11-15 20:53:34 +0800

    modify

--- dev/site/project/actions/content/person/profile.html.js
+++ dev/site/project/actions/content/person/profile.html.js
@@ -6,6 +6,41 @@ const T200HttpsForm = require('../../../../library/net/T200HttpsForm.js');
 const T200Person = require('../../../models/T200Person.js');
 const T200HomePerson = require('../../../biz/T200HomePerson.js');
 
+
+async function do_person_profile(request, response, cookie, session, resource) {
+    log(__filename, "do_person_profile");
+    let self = this;
+    let promise = new Promise(function(resolve, reject){
+        let person = new T200Person();
+        let HomePerson = new T200HomePerson();
+
+        if(HomePerson.verify_login(cookie, session)){
+            person.user_id = session.get("userid");
+            if(T200HttpsForm.verify_id(person.user_id)){
+                HomePerson.list(person.merge_select_by_id(person.user_id)).then(function(values){
+                    let view = new T200View(resource);
+                    let data = {};
+                    data.person = values[0];
+                    return view.render_file("content/person/profile.ejs", data).then(function(result){
+                        response.type("json");
+                        resolve(result);
+                    }, function(){
+                        reject();
+                    });
+                }, function(){
+                    reject();
+                });
+            }else{
+                reject();
+            }
+        }else{
+            reject();
+        }
+    });
+
+    return promise;
+}
+
 async function do_person_region(request, response, cookie, session, resource) {
     log(__filename, "do_person_region");
     let self = this;
@@ -75,5 +110,6 @@ async function do_person_region_save(request, response, cookie, session, resourc
 }
 
 global.action.use_post('/content/person/region', do_person_region);
+global.action.use_post('/content/person/profile', do_person_profile);
 global.action.use_post('/content/person/region/save', do_person_region_save);
 
